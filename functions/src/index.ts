62ca6b1ab7901bbf2b8391dc824ba70521de599c
Nikita Gordeev <nagordeev0106@yandex.ru>
2023-09-29 12:38:36 +0300

    github-client-server

---
+++ functions/src/index.ts
@@ -0,0 +1,92 @@
+import { defineSecret } from "firebase-functions/params";
+import { onRequest } from "firebase-functions/v2/https";
+
+//---PRODUCTION---
+const clientIdProduction = defineSecret("CLIENT_ID");
+const clientSecretProduction = defineSecret("CLIENT_SECRET");
+
+export const getGithubAccessToken = onRequest(
+  { secrets: [clientIdProduction, clientSecretProduction] },
+  async (request, response) => {
+    const params =
+      "?client_id=" +
+      clientIdProduction.value() +
+      "&client_secret=" +
+      clientSecretProduction.value() +
+      "&code=" +
+      request.query.code;
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        response.json(data);
+      })
+      .catch((error) => response.json(error));
+  }
+);
+
+//---DEVELOPED---
+const clientIdDeveloped = defineSecret("CLIENT_ID_DEVELOPED");
+const clientSecretDeveloped = defineSecret("CLIENT_SECRET_DEVELOPED");
+
+export const getGithubAccessTokenDeveloped = onRequest(
+  { secrets: [clientIdDeveloped, clientSecretDeveloped] },
+  async (request, response) => {
+    const params =
+      "?client_id=" +
+      clientIdDeveloped.value() +
+      "&client_secret=" +
+      clientSecretDeveloped.value() +
+      "&code=" +
+      request.query.code;
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        response.json(data);
+      })
+      .catch((error) => response.json(error));
+  }
+);
+
+//---DEPLOY---
+const clientIdDeploy = defineSecret("CLIENT_ID_DEPLOY");
+const clientSecretDeploy = defineSecret("CLIENT_SECRET_DEPLOY");
+
+export const getGithubAccessTokenDeploy = onRequest(
+  { secrets: [clientIdDeploy, clientSecretDeploy] },
+  async (request, response) => {
+    const params =
+      "?client_id=" +
+      clientIdDeploy.value() +
+      "&client_secret=" +
+      clientSecretDeploy.value() +
+      "&code=" +
+      request.query.code;
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        response.json(data);
+      })
+      .catch((error) => response.json(error));
+  }
+);
